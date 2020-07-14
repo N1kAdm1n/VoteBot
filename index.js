7be1cbf7cbf73e96f620510a99358cb7b4dbce77
N1kAdm1n <34167642+N1kAdm1n@users.noreply.github.com>
2020-07-14 14:44:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if((message.channel.id == "id первого канала")||(message.channel.id == "id второго канала")) {
+	if((message.channel.id == "731650898400641085")||(message.channel.id == "719201079568629760")) {
 	console.log(message.content);
 	message.react('➕');
 	message.react('➖');

8f45747e050755823ecf8d828ead983c7591d009
N1kAdm1n <34167642+N1kAdm1n@users.noreply.github.com>
2020-07-14 14:28:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿// JavaScript source code
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { prefix, token } = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if((message.channel.id == "id первого канала")||(message.channel.id == "id второго канала")) {
+	console.log(message.content);
+	message.react('➕');
+	message.react('➖');
+}
+});
+client.login(token);

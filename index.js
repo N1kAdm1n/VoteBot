f40a94e8b771d3b4914c6600c3e2940061a3d6c7
N1kAdm1n <34167642+N1kAdm1n@users.noreply.github.com>
2020-10-15 23:19:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-﻿// JavaScript source code
+// JavaScript source code
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
+
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -9,15 +13,17 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if((message.channel.id =="713101698150957136")||(message.channel.id =="718451613575938068")) {
-		console.log('Author: ' + message.author.username); console.log('Message: ' + message.content); message.react('➕'); message.react('➖');
+		console.log('Author: ' + message.author.username); console.log('Message: ' + message.content); 
+		message.react('➕'); message.react('➖');
 	}
-	else {
-        	if (message.content.startsWith(`${prefix}d12` && message.author.id =="278898242333442048")) {
-          		message.channel.send(`${message.author} you rolled ${getRandomInRange(1,12)}`);
-        	}
-		if (message.content.startsWith(`${prefix}d12` && message.author.id !="278898242333442048")) {
-          		message.channel.send(`${message.author} ты кто такой? Давай, до свидания!`);
-        	}
-	}
+    else if (message.content.startsWith(`${prefix}d12`) && (message.author.id =="278898242333442048" ||  message.author.id =="443722870791864340")) {
+			console.log('Author: ' + message.author.username); console.log('Message: ' + message.content);
+          	message.channel.send(`${message.author} you rolled ${getRandomInRange(1,12)}`);
+    }
+	else if (message.content.startsWith(`${prefix}d12`)) {
+			console.log('Author: ' + message.author.username); console.log('Message: ' + message.content);
+          	message.channel.send(`${message.author} ты кто такой? Давай, до свидания!`);
+    }
+	
 });
 client.login(token);

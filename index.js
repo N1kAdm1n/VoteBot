332435470105b65ccf1d0b0804bdc2047e3c2426
N1kAdm1n <34167642+N1kAdm1n@users.noreply.github.com>
2020-10-15 22:32:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ client.once('ready', () => {
 client.on('message', message => {
 	if((message.channel.id =="713101698150957136")||(message.channel.id =="718451613575938068")) {
 		console.log('Author: ' + message.author.username); console.log('Message: ' + message.content); message.react('➕'); message.react('➖');
-}
+	}
+	else {
+        	if (message.content.startsWith(`${prefix}d12` && message.author.id =="278898242333442048")) {
+          		message.channel.send(`${message.author} you rolled ${getRandomInRange(1,12)}`);
+        	}
+		if (message.content.startsWith(`${prefix}d12` && message.author.id !="278898242333442048")) {
+          		message.channel.send(`${message.author} ты кто такой? Давай, до свидания!`);
+        	}
+	}
 });
 client.login(token);
